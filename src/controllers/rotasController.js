771f7f2749f266c207b870c3e44aa79e5ac6efa1
igor-diego <igordiego237@gmail.com>
2022-07-22 21:05:11 -0300

    modificações na rota tips e create

--- src/controllers/rotasController.js
+++ src/controllers/rotasController.js
@@ -6,8 +6,6 @@ const rotas = (app) => {
     app.get('/tips', (req, res) => {
         const mostraDica = new Dicas
         res.json({"dicas": bd.msg[Math.floor(Math.random() * bd.msg.length)]})
-        // res.json({"dicas": msg.dica[Math.floor(Math.random() * msg.dica.lenght)]})
-        // res.json({"dicas": mostraDica.mostra()})
     })
 
     app.post('/create', (req, res) => {
